6cfbffef5bc8875ce7ce880d02eaf2ea10eb7dae
WildBabaloo <andphiltre@gmail.com>
2024-01-22 14:57:45 -0500

    Seed Data for organisations

--- lib/MyAppState.dart
+++ lib/MyAppState.dart
@@ -4,9 +4,11 @@ import 'entities/user.dart';
 
 class MyAppState extends ChangeNotifier {
   late List<User> users;
+  late List<Organisation> organisations;
 
   MyAppState() {
     users = seedDataUsers();
+    organisations = seedDataOrganisations();
   }
 
   List<User> seedDataUsers() {
@@ -17,10 +19,12 @@ class MyAppState extends ChangeNotifier {
       ];
   }
 
-  //List<Organisation> seedDataOrganisations() {
-    //return <Organisation>[
-      //Organisation(id: 1, userOwner: , organisationName: organisationName, isPublic: isPublic)
-    //]
-  //}
+  List<Organisation> seedDataOrganisations() {
+    return <Organisation>[
+      Organisation(id: 1, userOwner: User(id: 1, firstName: "Joe", lastName: "LeJoe", email: "joe@gmail.com", password: "joe"), organisationName: "Joe Bangers & Mash", isPublic: true),
+      Organisation(id: 2, userOwner: User(id: 2, firstName: "Meat", lastName: "Man", email: "TheMeatMan@gmail.com", password: 'meatman'), organisationName: "Meatmans's Shack", isPublic: true),
+      Organisation(id: 3, userOwner: User(id: 3, firstName: "Phillipe", lastName: "Gusto", email: "gustoP@gmail.com", password: "password"), organisationName: "The Gusto Show", isPublic: false)
+    ];
+  }
 
 }
