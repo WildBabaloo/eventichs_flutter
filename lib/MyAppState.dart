02f25b44fdfcb3a5694f6081a94f1d04abb69c86
WildBabaloo <andphiltre@gmail.com>
2024-01-22 14:35:30 -0500

    SeedData for users

--- lib/MyAppState.dart
+++ lib/MyAppState.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
+import 'entities/user.dart';
 
 class MyAppState extends ChangeNotifier {
-  
+  late List<User> users;
+
+  MyAppState() {
+    users = seedDataUsers();
+  }
+
+  List<User> seedDataUsers() {
+      return <User>[
+          User(id: 1, firstName: "Joe", lastName: "LeJoe", email: "joe@gmail.com", password: "joe"),
+          User(id: 2, firstName: "Meat", lastName: "Man", email: "TheMeatMan@gmail.com", password: 'meatman'),
+          User(id: 3, firstName: "Phillipe", lastName: "Gusto", email: "gustoP@gmail.com", password: "password")
+      ];
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:eventichs_flutter/MyAppState.dart';
+import 'package:eventichs_flutter/myAppState.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
